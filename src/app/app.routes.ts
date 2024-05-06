75bc811525e60eddf7e708f271cd96709305181a
Franco Griffone <pipokn@gmail.com>
2024-05-06 10:15:27 -0300

    cambios varios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //HOME
+    {
+        path: '',
+        component: MainComponent
+    },
+    //NOT FOUND
+    {
+        path: '**',
+        component: MainComponent
+    }
+];
---
+++ src/app/components/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [
+    ButtonModule
+  ],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+
+}
